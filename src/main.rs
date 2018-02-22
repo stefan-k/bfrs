90443f73c57e78f11152ef91f1effd984dffb28c
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-02-22 13:09:27 +0100

    renamed buffer to tape

--- src/main.rs
+++ src/main.rs
@@ -35,10 +35,10 @@ fn main() {
     let prog = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.".as_bytes();
     // let prog = "++[->+<]".as_bytes();
     // The buffer
-    let mut buf: VecDeque<Wrapping<u8>> = VecDeque::new();
-    // Start with a single `0` in the buffer. `Wrapping` is needed because we want to allow for
+    let mut tape: VecDeque<Wrapping<u8>> = VecDeque::new();
+    // Start with a single `0` in the tape. `Wrapping` is needed because we want to allow for
     // overflows.
-    buf.push_back(Wrapping(0));
+    tape.push_back(Wrapping(0));
     // State holds the position of the pointer
     let mut state = State::new();
     // a stack needed for matching `[` and `]`
@@ -52,40 +52,40 @@ fn main() {
             // Move right
             '>' => {
                 state.pos += 1;
-                match buf.get(state.pos) {
-                    // The buffer is not empty at the current position.
+                match tape.get(state.pos) {
+                    // The tape is not empty at the current position.
                     Some(_) => {}
-                    // We have exceeded the buffer and need to add another element
-                    None => buf.push_back(Wrapping(0)),
+                    // We have exceeded the tape and need to add another element
+                    None => tape.push_back(Wrapping(0)),
                 };
             }
             // Move left
             '<' => match state.pos {
-                // We are already at the beginning of the buffer, so we will just push to the
+                // We are already at the beginning of the tape, so we will just push to the
                 // front. Decreasing `state.pos` is not necessary.
                 0 => {
-                    buf.push_front(Wrapping(0));
+                    tape.push_front(Wrapping(0));
                 }
                 // Just move the pointer to the left
                 _ => state.pos -= 1,
             },
-            // Increase the value at the current buffer position. Allow for buffer overflows!
+            // Increase the value at the current tape position. Allow for buffer overflows!
             '+' => {
-                if let Some(elem) = buf.get_mut(state.pos) {
+                if let Some(elem) = tape.get_mut(state.pos) {
                     *elem += Wrapping(1);
                 }
             }
-            // Decrease the value at the current buffer position. Allow for buffer overflows!
+            // Decrease the value at the current tape position. Allow for buffer overflows!
             '-' => {
-                if let Some(elem) = buf.get_mut(state.pos) {
+                if let Some(elem) = tape.get_mut(state.pos) {
                     *elem -= Wrapping(1);
                 }
             }
-            // Print the `char` at the current buffer.
-            '.' => print!("{}", buf.get(state.pos).unwrap().0 as char),
+            // Print the `char` at the current tape position.
+            '.' => print!("{}", tape.get(state.pos).unwrap().0 as char),
             // We found a `[` which indicates the start of a loop
-            '[' => match buf.get(state.pos) {
-                // Value at current buffer is `0`, therefore we jump to the position after the
+            '[' => match tape.get(state.pos) {
+                // Value at current tape is `0`, therefore we jump to the position after the
                 // matching `]`.
                 Some(&Wrapping(0)) => {
                     // Keep track of all the other matching `[` and `]` that we encounter along the
@@ -107,7 +107,7 @@ fn main() {
                         };
                     }
                 }
-                // The value in the buffer is nonzero, therefore we just push the position of `[`
+                // The value in the tape is nonzero, therefore we just push the position of `[`
                 // to the ls stack and move on.
                 _ => {
                     ls.push(idx);
@@ -115,13 +115,13 @@ fn main() {
             },
             // We found a `]` which indicates the end of a loop
             ']' => {
-                match buf.get(state.pos) {
-                    // If the value in the buffer at the current position is nonzero, we move to
+                match tape.get(state.pos) {
+                    // If the value in the tape at the current position is nonzero, we move to
                     // the matching `[` which we have kept track of in the `ls` stack.
                     Some(c) if (*c).0 != 0 => {
                         idx = *ls.get(ls.len() - 1).unwrap();
                     }
-                    // The value at the current position of the buffer is zero, therefore we pop
+                    // The value at the current position of the tape is zero, therefore we pop
                     // the matching `[` off the `ls` stack and move on.
                     _ => {
                         ls.pop();
@@ -138,6 +138,6 @@ fn main() {
             break;
         }
     }
-    // Print the final buffer
-    println!("Buffer: {:?}", buf);
+    // Print the final tape
+    println!("Tape: {:?}", tape);
 }
